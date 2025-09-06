7bea4ca503cf685271092d8de3ce26ae2ad49200
piyan <piyanzin.nik2004@gmail.com>
2025-09-06 09:45:56 +0300

    Новый проект

---
+++ mysite/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        return render(request, 'contact.html', {'success': True})
+    return render(request, 'contact.html')
