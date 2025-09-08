e42657b0b69b1b6021970875fc302ee7d809b42c
cube-2025 <piyanzin.nik2004@gmail.com>
2025-09-08 09:31:06 +0300

    бд - исправить нужно

--- mysite/main/forms.py
+++ mysite/main/forms.py
@@ -1,79 +1,86 @@
 
 from django import forms
 
+# class UserForm(forms.Form):
+#     char_field = forms.CharField(
+#         label = 'Текстовое поле',
+#         max_length=100,
+#         required=True,
+#         help_text='Введите текст (макс 100)'
+#     )
+#     integer_field = forms.IntegerField(
+#         label='Целое число',
+#         min_value=0,
+#         max_value=100,
+#         required=True,
+#         help_text='Введите число от 0 до 100'
+#     )
+#     float_field = forms.FloatField(
+#         label='Вещественное число',
+#         min_value=0.0,
+#         max_value=100.0,
+#         required=True,
+#         help_text='Введите число от 0 до 100'
+#     )
+#     boolean_field = forms.BooleanField(
+#         label='Флажок',
+#         required=True,
+#         help_text='Отметьте'
+#     )
+#     data_field = forms.DateField(
+#         label='Дата',
+#         input_formats=['%d-%m-%Y'],
+#         required=True,
+#         help_text='Введи дату'
+#     )
+#     email_field = forms.EmailField(
+#         label='email',
+#         max_length =254,
+#         help_text='введите почту'
+#     )
+#     file_field = forms.FileField(
+#         label='Файл',
+#         required=False,
+#         help_text='Выберите файл для загрузки'
+#     )
+#     image_field = forms.ImageField(
+#         label='Фото',
+#         required=False,
+#         help_text='Выберите фото для загрузки'
+#     )
+#     CHOICES = [
+#         ('option1', 'Вар 1'),
+#         ('option2', 'Вар 2'),
+#         ('option3', 'Вар 3')
+#     ]
+#     choice_field = forms.ChoiceField(
+#         label='Выбор вариантов',
+#         choices=CHOICES,
+#         required=True,
+#         help_text='Выберите один вариант'
+#     )
+#     multiple_choice_field = forms.MultipleChoiceField(
+#         label='Множественный выбор',
+#         choices=CHOICES,
+#         required=False,
+#         help_text='Выберите несколько вариантов'
+#     )
+#     radio_select_field = forms.ChoiceField(
+#         label='radio',
+#         choices=CHOICES,
+#         widget=forms.RadioSelect,
+#         required=True,
+#         help_text='Радиокнопки'
+#     )
+
 class UserForm(forms.Form):
-    char_field = forms.CharField(
-        label = 'Текстовое поле',
-        max_length=100,
-        required=True,
-        help_text='Введите текст (макс 100)'
+    name = forms.CharField(
+        label= 'Имя клиента',
+        widget = forms.TextInput(attrs={"class":"myfield"})
     )
-    integer_field = forms.IntegerField(
-        label='Целое число',
-        min_value=0,
-        max_value=100,
-        required=True,
-        help_text='Введите число от 0 до 100'
-    )
-    float_field = forms.FloatField(
-        label='Вещественное число',
-        min_value=0.0,
-        max_value=100.0,
-        required=True,
-        help_text='Введите число от 0 до 100'
-    )
-    boolean_field = forms.BooleanField(
-        label='Флажок',
-        required=True,
-        help_text='Отметьте'
-    )
-    data_field = forms.DateField(
-        label='Дата',
-        input_formats=['%d-%m-%Y'],
-        required=True,
-        help_text='Введи дату'
-    )
-    email_field = forms.EmailField(
-        label='email',
-        max_length =254,
-        help_text='введите почту'
-    )
-    file_field = forms.FileField(
-        label='Файл',
-        required=False,
-        help_text='Выберите файл для загрузки'
-    )
-    image_field = forms.ImageField(
-        label='Фото',
-        required=False,
-        help_text='Выберите фото для загрузки'
-    )
-    CHOICES = [
-        ('option1', 'Вар 1'),
-        ('option2', 'Вар 2'),
-        ('option3', 'Вар 3')
-    ]
-    choice_field = forms.ChoiceField(
-        label='Выбор вариантов',
-        choices=CHOICES,
-        required=True,
-        help_text='Выберите один вариант'
-    )
-    multiple_choice_field = forms.MultipleChoiceField(
-        label='Множественный выбор',
-        choices=CHOICES,
-        required=False,
-        help_text='Выберите несколько вариантов'
-    )
-    radio_select_field = forms.ChoiceField(
-        label='radio',
-        choices=CHOICES,
-        widget=forms.RadioSelect,
-        required=True,
-        help_text='Радиокнопки'
-    )
-
-
+    age = forms.IntegerField(
+        label = "Возраст клиента",
+        widget=forms.NumberInput(attrs={"class":"myfield"}))
 
 
 
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserForm
+from .models import Person
 def index(request):
     my_text = 'Прошли формы',
-    context = {"my_text": my_text}
+    people_kol = Person.object_person.count()
+    context = {"my_text": my_text, "person_kol": people_kol}
     return render(request, 'index.html', context)
 
+def my_form(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    people = Person.object_person.all()
+    form = UserForm()
+
+    context = {"form": form, "people": people}
+    return render(request, 'my_form.html', context)
 def about(request):
     return render(request, 'about.html')
 
-def my_form(request):
-    my_form = UserForm()
-    context = {"form": my_form}
-    return render(request, 'my_form.html', context)
+# def my_form(request):
+#     my_form = UserForm()
+#     context = {"form": my_form}
+#     return render(request, 'my_form.html', context)
 
 def contact(request):
     if request.method == 'POST':
